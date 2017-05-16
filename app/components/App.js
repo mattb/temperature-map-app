412c322edd88128d7b338f653ee9d2ed44adf195
Matt Biddulph <mb@hackdiary.com>
2017-05-15 20:19:23 -0700

    Derive PNG URL from JSON data. Make more efforts to always fetch a fresh JSON file.

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,9 @@
 import codePush from 'react-native-code-push';
 import React, { Component } from 'react';
-import { StyleSheet, Image, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import TemperatureLabels from './TemperatureLabels';
 
 const styles = StyleSheet.create({
-  map: {
-    width: 375,
-    height: 667,
-    resizeMode: 'cover'
-  },
   labels: {
     width: 375,
     height: 667
@@ -26,7 +21,7 @@ class App extends Component {
       this.setState({
         hour: (new Date().getTime() / 1000 / 60 / 60).toFixed(0)
       });
-    }, 1000 * 60 * 60);
+    }, 1000 * 60);
     const displayModes = ['name', 'temp']; // 'all', 'none'
     let displayModeIndex = 0;
     this.viewClick = () => {
@@ -40,21 +35,11 @@ class App extends Component {
   render() {
     return (
       <TouchableOpacity onPress={this.viewClick} activeOpacity={1}>
-        <View style={styles.container}>
-          <Image
-            style={styles.map}
-            source={{
-              uri: `https://tempmap.s3.amazonaws.com/temps.png?${this.state.hour}`,
-              cache: 'reload'
-            }}
-          >
-            <TemperatureLabels
-              style={styles.labels}
-              hour={this.state.hour}
-              displayMode={this.state.displayMode}
-            />
-          </Image>
-        </View>
+        <TemperatureLabels
+          style={styles.labels}
+          hour={this.state.hour}
+          displayMode={this.state.displayMode}
+        />
       </TouchableOpacity>
     );
   }
--- app/components/TemperatureLabels.js
+++ app/components/TemperatureLabels.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 import moment from 'moment';
 
 const stylePropType = require('react-style-proptype');
+
+const styles = StyleSheet.create({
+  map: {
+    width: 375,
+    height: 667,
+    resizeMode: 'cover'
+  }
+});
 
 class TemperatureLabels extends Component {
   constructor(props) {
@@ -20,6 +28,7 @@ class TemperatureLabels extends Component {
       }));
     };
     this.f = c => 32 + parseFloat(c, 10) * 9.0 / 5.0;
+    this.fontSize = () => (this.props.displayMode === 'temp' ? 13 : 10);
     this.format = place => {
       const name = place.name;
       const temp = `${Math.round(this.f(place.temp_in_c))}`;
@@ -57,13 +66,15 @@ class TemperatureLabels extends Component {
     );
     fetch(`https://tempmap.s3.amazonaws.com/temps.json?${this.props.hour}`, {
       headers: {
-        'Cache-Control': 'no-cache'
+        pragma: 'no-cache',
+        'cache-control': 'no-cache'
       }
     })
       .then(response => response.json())
       .then(data => {
         this.setState({
           ...data,
+          image_url: `https://tempmap.s3.amazonaws.com/${data.png}`,
           when: moment(data.timestamp).local().format('MMMM Do YYYY [at] ha')
         });
       });
@@ -71,64 +82,71 @@ class TemperatureLabels extends Component {
   render() {
     if (this.state.places) {
       return (
-        <View style={this.props.style}>
-          <Text
-            style={{
-              fontSize: 10,
-              left: 10,
-              bottom: 10,
-              color: 'rgba(0, 0, 0, 0.7)',
-              backgroundColor: 'rgba(230, 230, 230, 0.7)',
-              position: 'absolute',
-              textAlign: 'left',
-              padding: 3
+        <View style={styles.container}>
+          <Image
+            style={styles.map}
+            source={{
+              uri: this.state.image_url
             }}
           >
-            min
-            {' '}
-            {this.f(this.state.min_in_c).toFixed(0)}
-            {' '}
-            average
-            {' '}
-            {this.f(this.state.average_in_c).toFixed(0)}
-            {' '}
-            max
-            {' '}
-            {this.f(this.state.max_in_c).toFixed(0)}
-            {'\n'}
-            last updated
-            {' '}
-            {this.state.when}
-          </Text>
-          {this.state.places.map(place => {
-            let offsetX = 0;
-            let offsetY = 0;
-            let color = 'rgba(0, 0, 0, 0)';
-            if (this.state.dimensions[place.name]) {
-              offsetX = this.state.dimensions[place.name][0] / 2;
-              offsetY = this.state.dimensions[place.name][1] / 2;
-              color = 'rgba(0, 0, 0, 0.6)';
-            }
-            return (
-              <Text
-                key={place.name}
-                onLayout={this.onLayout(place.name)}
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0)',
-                  color,
-                  fontSize: 10,
-                  fontWeight: 'bold',
-                  left: parseInt(place.x, 10) / 2 - offsetX,
-                  top: parseInt(place.y, 10) / 2 - offsetY,
-                  position: 'absolute',
-                  textAlign: 'center',
-                  width: 65
-                }}
-              >
-                {this.format(place)}
-              </Text>
-            );
-          })}
+            <Text
+              style={{
+                fontSize: 10,
+                left: 10,
+                bottom: 10,
+                color: 'rgba(0, 0, 0, 0.7)',
+                backgroundColor: 'rgba(230, 230, 230, 0.7)',
+                position: 'absolute',
+                textAlign: 'left',
+                padding: 3
+              }}
+            >
+              min
+              {' '}
+              {this.f(this.state.min_in_c).toFixed(0)}
+              {' '}
+              average
+              {' '}
+              {this.f(this.state.average_in_c).toFixed(0)}
+              {' '}
+              max
+              {' '}
+              {this.f(this.state.max_in_c).toFixed(0)}
+              {'\n'}
+              last updated
+              {' '}
+              {this.state.when}
+            </Text>
+            {this.state.places.map(place => {
+              let offsetX = 0;
+              let offsetY = 0;
+              let color = 'rgba(0, 0, 0, 0)';
+              if (this.state.dimensions[place.name]) {
+                offsetX = this.state.dimensions[place.name][0] / 2;
+                offsetY = this.state.dimensions[place.name][1] / 2;
+                color = 'rgba(0, 0, 0, 0.6)';
+              }
+              return (
+                <Text
+                  key={place.name}
+                  onLayout={this.onLayout(place.name)}
+                  style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    color,
+                    fontSize: this.fontSize(),
+                    fontWeight: 'bold',
+                    left: parseInt(place.x, 10) / 2 - offsetX,
+                    top: parseInt(place.y, 10) / 2 - offsetY,
+                    position: 'absolute',
+                    textAlign: 'center',
+                    width: 65
+                  }}
+                >
+                  {this.format(place)}
+                </Text>
+              );
+            })}
+          </Image>
         </View>
       );
     }
