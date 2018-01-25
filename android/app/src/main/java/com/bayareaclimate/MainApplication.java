fad3a80b43fe4eeae11244294cb9a44973ba3fff
Matt Biddulph <mb@hackdiary.com>
2018-01-25 15:00:52 -0800

    rename

--- android/app/src/main/java/com/bayareaclimate/MainApplication.java
+++ android/app/src/main/java/com/bayareaclimate/MainApplication.java
@@ -1,4 +1,4 @@
-package com.marine;
+package com.bayareaclimate;
 
 import android.app.Application;
 
--- ios/BayAreaClimateTests/BayAreaClimateTests.m
+++ ios/BayAreaClimateTests/BayAreaClimateTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface marineTests : XCTestCase
+@interface BayAreaClimateTests : XCTestCase
 
 @end
 
-@implementation marineTests
+@implementation BayAreaClimateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
